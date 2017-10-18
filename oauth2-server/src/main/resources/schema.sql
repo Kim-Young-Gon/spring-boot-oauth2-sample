8ade9855ca2e0fcbe25b18f9fd60655def64e94c
Kim-Young-Gon <kimyg3174@gmail.com>
2017-10-18 15:20:54 +0900

    MySql 용 테이블 생성 추가
    PK 오류 관련 내용 추가

--- oauth2-server/src/main/resources/schema.sql
+++ oauth2-server/src/main/resources/schema.sql
@@ -50,9 +50,28 @@ create table oauth_approvals (
 	lastModifiedAt TIMESTAMP
 );
 
+-- customized oauth_client_details table
+create table ClientDetails (
+  appId VARCHAR(256) PRIMARY KEY,
+  resourceIds VARCHAR(256),
+  appSecret VARCHAR(256),
+  scope VARCHAR(256),
+  grantTypes VARCHAR(256),
+  redirectUrl VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additionalInformation VARCHAR(4096),
+  autoApproveScopes VARCHAR(256)
+);
+
 -- used in tests that use MySql
+SET GLOBAL INNODB_FILE_FORMAT_MAX=Barracuda;
+SET GLOBAL INNODB_FILE_FORMAT=Barracuda;
+SET GLOBAL INNODB_LARGE_PREFIX=ON;
+
 create table oauth_client_details (
-  client_id VARCHAR(256) PRIMARY KEY,
+  client_id VARCHAR(256),
   resource_ids VARCHAR(256),
   client_secret VARCHAR(256),
   scope VARCHAR(256),
@@ -62,8 +81,9 @@ create table oauth_client_details (
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additional_information VARCHAR(4096),
-  autoapprove VARCHAR(256)
-);
+  autoapprove VARCHAR(256),
+  PRIMARY KEY (client_id)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 create table oauth_client_token (
   token_id VARCHAR(256),
@@ -90,7 +110,8 @@ create table oauth_refresh_token (
 );
 
 create table oauth_code (
-  code VARCHAR(256), authentication BLOB
+  code VARCHAR(256),
+  authentication BLOB
 );
 
 create table oauth_approvals (
@@ -103,17 +124,3 @@ create table oauth_approvals (
 );
 
 
--- customized oauth_client_details table
-create table ClientDetails (
-  appId VARCHAR(256) PRIMARY KEY,
-  resourceIds VARCHAR(256),
-  appSecret VARCHAR(256),
-  scope VARCHAR(256),
-  grantTypes VARCHAR(256),
-  redirectUrl VARCHAR(256),
-  authorities VARCHAR(256),
-  access_token_validity INTEGER,
-  refresh_token_validity INTEGER,
-  additionalInformation VARCHAR(4096),
-  autoApproveScopes VARCHAR(256)
-);

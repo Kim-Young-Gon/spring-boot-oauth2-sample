50657196eee7a18598b89a7463e06d1d4e9d9222
Kim-Young-Gon <kimyg3174@gmail.com>
2017-10-23 16:17:52 +0900

    javamelody 추가
    javamelody 추가
    DB 설정 변경 : tomcat dbcp 사용

--- oauth2-server/src/main/java/com/example/config/OAuth2Configuration.java
+++ oauth2-server/src/main/java/com/example/config/OAuth2Configuration.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +11,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.security.access.method.P;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 @EnableJpaRepositories(
@@ -23,10 +27,20 @@ import javax.sql.DataSource;
         transactionManagerRef = "userTransactionManager"
 )
 public class OAuth2Configuration {
+    @Primary
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.oauth2")
+    @ConfigurationProperties(prefix = "oauth2.datasource")
+    @Qualifier("primary")
+    public DataSourceProperties oauth2DataSourceProp() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "oauth2.datasource.tomcat")
+    @Qualifier("primary")
     public DataSource oauth2DataSource() {
-        return DataSourceBuilder.create().build();
+        return oauth2DataSourceProp().initializeDataSourceBuilder().build();
     }
 
     @Bean
@@ -39,19 +53,29 @@ public class OAuth2Configuration {
         return new JwtTokenStore(jwtAccessTokenConverter());
     }
 
-    @Bean
     @Primary
+    @Bean
+    @Qualifier("primary")
     public JdbcClientDetailsService jdbcClientDetailsService() {
         return new JdbcClientDetailsService(oauth2DataSource());
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.user")
-    public DataSource userDataSource() {
-        return DataSourceBuilder.create().build();
+    @ConfigurationProperties(prefix = "user.datasource")
+    @Qualifier("user")
+    public DataSourceProperties userDataSourceProp() {
+        return new DataSourceProperties();
     }
 
     @Bean
+    @ConfigurationProperties(prefix = "user.datasource.tomcat")
+    @Qualifier("user")
+    public DataSource userDataSource() {
+        return userDataSourceProp().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Qualifier("user")
     public LocalContainerEntityManagerFactoryBean userEntityManager() {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(userDataSource());
@@ -62,6 +86,7 @@ public class OAuth2Configuration {
     }
 
     @Bean
+    @Qualifier("user")
     public PlatformTransactionManager userTransactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(userEntityManager().getObject());

8f160acf1596e96e9149df8018cc71e0eb8e7e49
Kim-Young-Gon <kimyg3174@gmail.com>
2017-10-31 11:22:24 +0900

    API 서버 접근 허용 범위 수정
    OAuth2SecurityExpressionMethods 관련 내용 추가

--- api-server/src/main/java/com/example/config/ResourceServerConfig.java
+++ api-server/src/main/java/com/example/config/ResourceServerConfig.java
@@ -10,10 +10,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        String memberListAccess = "(#oauth2.hasScope('read') or #oauth2.hasScope('ALL')) and hasAnyRole('ROLE_CLIENTAPP', 'ROLE_USER')";
+        String memeberActionAccess = "(#oauth2.hasScope('write') or #oauth2.hasScope('ALL')) and hasRole('ROLE_USER')";
         http.headers().frameOptions().disable();
         http.authorizeRequests()
-                .antMatchers("/members").access("#oauth2.hasScope('read') or #oauth2.hasScope('ALL')")
-                .antMatchers("/members/**").access("#oauth2.hasScope('write') or #oauth2.hasScope('ALL')")
+                // OAuth2SecurityExpressionMethods 방식
+                .antMatchers("/members").access(memberListAccess)
+                .antMatchers("/members/**").access(memeberActionAccess)
+                // Authorities 확인 방식
+//                .antMatchers("/members/**").hasAnyAuthority("ROLE_CLIENTAPP", "ROLE_USER")
+                // 전체 허용
 //				.antMatchers("/members", "/members/**").permitAll()
                 .anyRequest().authenticated();
     }
